c94757ee3823bc876690b9dbc09e16f5c910a74e
Andrei Cozma <andreig992@gmail.com>
2020-11-21 22:33:47 -0600

    Updates for honors requirements

--- script.js
+++ script.js
@@ -20,11 +20,11 @@ function openNav() {
 }
 
 function openNavAction() {
-    if (window.innerWidth / window.innerHeight < 1) {
+    if (window.innerWidth / window.innerHeight < .75) {
         document.getElementById("mySidenav").style.width = "100%";
-    } else if (window.innerWidth / window.innerHeight < 1.2) {
+    } else if (window.innerWidth / window.innerHeight < 1) {
         document.getElementById("mySidenav").style.width = "50%";
-    } else if (window.innerWidth / window.innerHeight < 1.7) {
+    } else if (window.innerWidth / window.innerHeight < 1.25) {
         document.getElementById("mySidenav").style.width = "40%";
     } else {
         document.getElementById("mySidenav").style.width = "30%";
