c08e45e1947c6fa157895febcff0e990c3ff3ada
Andrei Cozma <me@andreicozma.com>
2022-06-28 18:19:23 -0500

    Added data for seminars

---
+++ src/data/seminars/index.tsx
@@ -0,0 +1,13 @@
+import Fall2020 from "./Fall2020"
+import Spring2021 from "./Spring2021"
+import Fall2021 from "./Fall2021"
+import Spring2022 from "./Spring2022"
+
+const DataSeminars = [
+	Spring2022,
+	Fall2021,
+	Spring2021,
+	Fall2020
+]
+
+export default DataSeminars
