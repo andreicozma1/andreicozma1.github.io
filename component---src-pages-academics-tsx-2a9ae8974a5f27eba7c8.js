17257b7d68249673fd162bbad98a87de71331adc
Andrei Cozma <me@andreicozma.com>
2022-07-08 20:06:06 -0500

    Updates

---
+++ component---src-pages-academics-tsx-2a9ae8974a5f27eba7c8.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpersonal_portfolio=self.webpackChunkpersonal_portfolio||[]).push([[834],{6733:function(e,a,r){r.r(a);var t=r(7294),n=r(6668),o=r(6154);a.default=function(){var e=(0,o.q)("Academics");return t.createElement(n.Z,{data:e})}}}]);
+//# sourceMappingURL=component---src-pages-academics-tsx-2a9ae8974a5f27eba7c8.js.map
---
+++ component---src-pages-resume-tsx-8acb781e79f689690e10.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpersonal_portfolio=self.webpackChunkpersonal_portfolio||[]).push([[753],{1391:function(e,r,t){t.r(r);var a=t(7294),n=t(6668),o=t(6154);r.default=function(){var e=(0,o.q)("Resume");return a.createElement(n.Z,{data:e})}}}]);
+//# sourceMappingURL=component---src-pages-resume-tsx-8acb781e79f689690e10.js.map
