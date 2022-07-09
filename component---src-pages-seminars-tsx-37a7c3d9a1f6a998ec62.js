bd55496058822beb8768f8ed0c8ec0aeee07d1af
Andrei Cozma <me@andreicozma.com>
2022-07-09 00:52:56 -0500

    Updates

---
+++ component---src-pages-seminars-tsx-37a7c3d9a1f6a998ec62.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpersonal_portfolio=self.webpackChunkpersonal_portfolio||[]).push([[508],{7320:function(e,r,a){a.r(r);var n=a(7294),t=a(7480),o=a(3708);r.default=function(){var e=(0,o.q)("Seminars");return n.createElement(t.Z,{data:e})}}}]);
+//# sourceMappingURL=component---src-pages-seminars-tsx-37a7c3d9a1f6a998ec62.js.map
