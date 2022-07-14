e31eb34179f068e5520d629680be349fb4edddcd
Andrei Cozma <me@andreicozma.com>
2022-07-13 21:59:48 -0500

    Update photography page columns

--- src/components/Photos.tsx
+++ src/components/Photos.tsx
@@ -1,8 +1,10 @@
 import React from "react"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { graphql, StaticQuery } from "gatsby"
-import { Typography } from "@mui/material"
 import { Masonry } from "@mui/lab"
+import { PageSection } from "./page/PageSectionTemplate"
+import { useMediaQuery } from "@mui/material"
+import theme from "../config/theme"
 
 const Photos = () => {
 
@@ -31,17 +33,19 @@ const Photos = () => {
 						}
 					`}
 		render={data => {
+			const matchesMd = useMediaQuery(theme.breakpoints.up("md"))
+			const matchesLg = useMediaQuery(theme.breakpoints.up("lg"))
+			let columns = matchesMd ? (matchesLg ? 3 : 2) : 1
 			const photos = data.allGooglePhotosAlbum.nodes
-			return photos.map((albumNode: any) => (<div key={albumNode.title}>
-					<Typography variant="h4">{albumNode.title}</Typography>
-					<Masonry columns={3} spacing={2}>
+			return photos.map((albumNode: any) => (<PageSection title={albumNode.title} key={albumNode.title}>
+					<Masonry columns={columns} spacing={1}>
 						{albumNode.photos.map((photoNode: any) => {
-							return <div style={{ width: 500 }}>
+							return <div>
 								<GatsbyImage image={getImage(photoNode.file)} alt="Photo"/>
 							</div>
 						})}
 					</Masonry>
-				</div>
+				</PageSection>
 
 			))
 		}}
