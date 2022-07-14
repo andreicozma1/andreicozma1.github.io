448333493baebd93160b6ab73cda08ec24dc189a
andreicozma1 <andreicozma1@users.noreply.github.com>
2022-07-14 02:09:22 +0000

    deploy: d65ffb951d41801f2fca25f7e00cc55bd230ab0f

---
+++ component---src-pages-resume-tsx-ce0b9d4ba3c4eb722cc3.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpersonal_portfolio=self.webpackChunkpersonal_portfolio||[]).push([[753],{1391:function(e,r,o){o.r(r);var n=o(7294),t=o(5100),a=o(6369);r.default=function(){var e=(0,a.q)("Resume");return n.createElement(t.Z,{pageProps:e})}}}]);
+//# sourceMappingURL=component---src-pages-resume-tsx-ce0b9d4ba3c4eb722cc3.js.map
