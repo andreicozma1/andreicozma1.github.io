5240047b46eb16c24bb3f9f95c77b502ef284aff
Andrei Cozma <me@andreicozma.com>
2022-07-16 18:11:41 -0500

    Temporarily remove contentful integration

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -14,14 +14,14 @@ const config: GatsbyConfig = {
     // Learn more at: https://gatsby.dev/graphql-typegen
     graphqlTypegen: true,
     plugins: [
-        {
-            resolve: `gatsby-source-contentful`,
-            options: {
-                spaceId: process.env.CONTENTFUL_SPACE_ID,
-                // Learn about environment variables: https://gatsby.dev/env-vars
-                accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-            },
-        },
+        // {
+        //     resolve: `gatsby-source-contentful`,
+        //     options: {
+        //         spaceId: process.env.CONTENTFUL_SPACE_ID,
+        //         // Learn about environment variables: https://gatsby.dev/env-vars
+        //         accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        //     },
+        // },
         {
             resolve: `gatsby-plugin-google-gtag`,
             options: {
