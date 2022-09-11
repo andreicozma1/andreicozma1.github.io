80f92cafe31961b80aaa55de93f891f2ce51514d
Andrei Cozma <me@andreicozma.com>
2022-09-10 21:10:07 -0400

    Quick update/fix

--- src/pages/photos.tsx
+++ src/pages/photos.tsx
@@ -5,7 +5,7 @@ Github Repository: https://github.com/andreicozma1/andreicozma1.github.io
 */
 import * as React from "react"
 import Page from "../components/PageElements/Page"
-import PhotosSection from "../components/Custom/PhotosSection"
+// import PhotosSection from "../components/Custom/PhotosSection"
 import { usePage } from "../Utils"
 import SEO from "../components/SEO"
 
