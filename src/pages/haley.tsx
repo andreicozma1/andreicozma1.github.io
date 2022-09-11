40f552c6135091658f27e43f266a7009d50294b2
Andrei Cozma <me@andreicozma.com>
2022-09-10 21:05:25 -0400

    Quick update/fix

---
+++ src/pages/haley.tsx
@@ -0,0 +1,17 @@
+/*
+Author: Andrei Cozma
+Website: https://andreicozma.com
+Github Repository: https://github.com/andreicozma1/andreicozma1.github.io
+*/
+import * as React from "react"
+import Page from "../components/PageElements/Page"
+import { usePage } from "../Utils"
+
+const Projects = () => {
+
+	return <Page>
+
+	</Page>
+}
+
+export default Projects
