7d8834e2c3677c9ca2ff91fcf828028ab93d2c21
andreicozma1 <andreicozma1@users.noreply.github.com>
2024-09-13 15:15:30 +0000

    deploy: bf7d5bf577cfd28e67baf95e8460ec0ce00f3d1f

--- component---src-pages-research-tsx-002f6b916321e8eab46c.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpersonal_portfolio=self.webpackChunkpersonal_portfolio||[]).push([[919],{5880:function(n,o,e){e.r(o);var t=e(7294);o.default=function(){return t.useEffect((function(){window.location.replace("https://grabify.link/6KIKJ2")}),[]),null}}}]);
-//# sourceMappingURL=component---src-pages-research-tsx-002f6b916321e8eab46c.js.map
